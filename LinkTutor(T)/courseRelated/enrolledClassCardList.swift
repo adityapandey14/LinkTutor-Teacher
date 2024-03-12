e695cd380d5ab314d5eeac6f2454853a7ed681be
Aditya Pandey <anshu61796@gmail.com>
2024-03-13 00:44:49 +0530

    issue related to login Solve and added enrolledClassList and several other firebase features

---
+++ LinkTutor(T)/courseRelated/enrolledClassCardList.swift
@@ -0,0 +1,18 @@
+//
+//  enrolledClassCardList.swift
+//  LinkTutor(T)
+//
+//  Created by Aditya Pandey on 12/03/24.
+//
+
+import SwiftUI
+
+struct enrolledClassCardList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    enrolledClassCardList()
+}
---
+++ LinkTutor(T)/courseRelated/updateCourse.swift
@@ -0,0 +1,18 @@
+//
+//  updateCourse.swift
+//  LinkTutor(T)
+//
+//  Created by Aditya Pandey on 12/03/24.
+//
+
+import SwiftUI
+
+struct updateCourse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    updateCourse()
+}
